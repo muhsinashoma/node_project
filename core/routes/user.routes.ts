b9eb57ab0a534cdd5cecb236473a679df5cdc7ea
muhsinashoma <muhsina.akter2@gmail.com>
2025-02-08 11:23:55 +0600

    To Add Git Commit Command

--- core/routes/user.routes.ts
+++ core/routes/user.routes.ts
@@ -5,7 +5,13 @@ const router = Router();
 
 router.get('/', UserController.getAll);
 router.get('/create', UserController.createForm);
-router.post('/', UserController.create);                  //2nd start from here
+
+
+//2nd start from here
+
+router.post('/', UserController.create);                 
+
+
 router.get('/:id', UserController.show);
 router.get('/:id/edit', UserController.editForm);
 router.post('/:id/update', UserController.update);
